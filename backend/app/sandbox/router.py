9d7fffeca42a24bbe2cf8b68729a9d08a77e8701
b2k4rys <bekarys20050812@gmail.com>
2026-06-05 14:51:08 +0500

    add time limiting

--- backend/app/sandbox/router.py
+++ backend/app/sandbox/router.py
@@ -13,14 +13,22 @@ async def execute_docker(file: UploadFile = File()):
 
     with open(f'sample_{random_id}.py', 'w') as file:
         file.write(f"\n{text}")
+    try:
 
-    subprocess.run([
-        'docker', 'run', '--name', 'output', '--network', 'none',
-        '-v', f'{os.getcwd()}/sample_{random_id}.py:/app/sample.py',
-        'test'
-    ])
+        subprocess.run([
+            'docker', 'run', '--name', 'output', '--network', 'none',
+            '--memory', '128m', '--cpus', '0.5',
+            '-v', f'{os.getcwd()}/sample_{random_id}.py:/app/sample.py',
+            'test'
+        ], timeout=10)
+    except subprocess.TimeoutExpired:
+        subprocess.run(['docker', 'rm', '-f', 'output'])
+        os.remove(f'{os.getcwd()}/sample_{random_id}.py')
+        return "Time Limit exceeded"
+
     logs = subprocess.run(['docker', 'logs', 'output'], capture_output=True, text=True)
     subprocess.run(['docker', 'rm', '-f', 'output'])
+    os.remove(f'{os.getcwd()}/sample_{random_id}.py')
     print('here is logs', logs)
     return logs.stdout
 
