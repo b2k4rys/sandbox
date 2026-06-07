cba074fc35dbdeff8a8498b9cdf073ac84f4f087
b2k4rys <bekarys20050812@gmail.com>
2026-06-07 20:48:43 +0500

    start working on auth dependency injection

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -1,8 +1,12 @@
-from dns.dnssecalgs import algorithms
 from jose import jwt
 from datetime import datetime, timedelta, UTC
 from pwdlib import PasswordHash
 import settings
+from .models import User
+from fastapi import APIRouter, Depends
+from database import get_db
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 password_hash = PasswordHash.recommended()
 
@@ -16,3 +20,16 @@ def create_access_token(data: dict) -> str:
     payload = data.copy()
     payload['exp'] = datetime.now(UTC) + timedelta(minutes=int(settings.token_expire_minutes))
     return jwt.encode(payload, str(settings.SECRET_KEY), algorithm='HS256')
+
+def decode_jwt(token: str):
+    print(repr(token))
+    return jwt.decode(token, key=str(settings.SECRET_KEY), algorithms=['HS256'])
+
+
+async def get_current_user(token: str, db: AsyncSession = Depends(get_db)):
+    user_id = decode_jwt(token)
+    if user_id:
+        result = await db.execute(select(User).filter_by(id=user_id))
+        user = result.scalars().first()
+        return user
+    return "not valid"
--- backend/app/sandbox/router.py
+++ backend/app/sandbox/router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Depends
 from .service import execute_code
+from app.auth.service import get_current_user
+from app.auth.models import User
 sandbox_router = APIRouter(prefix='/sandbox')
 
 @sandbox_router.post('/execute')
-async def execute_docker(file: UploadFile = File()):
+async def execute_docker(file: UploadFile = File(), user: User = Depends(get_current_user)):
     output = await execute_code(file)
     return output
