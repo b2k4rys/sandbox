2d4a519faa64accc3c60b3b1ebe35963de79b992
b2k4rys <bekarys20050812@gmail.com>
2026-06-09 21:44:20 +0500

    working version

--- backend/app/sandbox/worker.py
+++ backend/app/sandbox/worker.py
@@ -4,7 +4,7 @@ import os
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 redis_url = 'redis://localhost:6379/0'
-celery = Celery(__name__)
+celery = Celery(__name__, include=["app.sandbox.service"])
 
 celery.conf.broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379")
