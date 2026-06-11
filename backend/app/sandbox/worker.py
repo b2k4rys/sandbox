80510f691fab8bc09c587d70516102b7273271fb
b2k4rys <bekarys20050812@gmail.com>
2026-06-11 11:10:11 +0500

    move celery

--- backend/app/sandbox/worker.py
+++ backend/app/sandbox/worker.py
@@ -1,6 +1,8 @@
 from celery import Celery
 import os
+import settings
 celery = Celery(__name__, include=["app.sandbox.service"])
 
-celery.conf.broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+
+celery.conf.broker_url = os.getenv(settings.CELERY_BROKER_URL, "redis://localhost:6379")
+celery.conf.result_backend = os.getenv(settings.CELERY_RESULT_BACKEND, "redis://localhost:6379")
