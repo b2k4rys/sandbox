6253d7ba7a696582b51e8defef20c1ca43754146
b2k4rys <bekarys20050812@gmail.com>
2026-06-23 23:23:17 +0500

    add simple rate limiter (fixed window) via redis

--- backend/app/sandbox/rate_limiter.py
+++ backend/app/sandbox/rate_limiter.py
@@ -3,4 +3,6 @@ from ipaddress import ip_address
 
 class RateLimiter:
     def __init__(self, ip_address):
-        self.ip_adress = ip_address
+        self.ip_address = ip_address
+        self.limit = 5
+        self.seconds = 60
--- backend/app/sandbox/router.py
+++ backend/app/sandbox/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File, Depends, Request
+from fastapi import APIRouter, UploadFile, File, Depends, Request, HTTPException
 from fastapi.params import Depends, Body
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.sandbox.schemas import CeleryTaskResponse, TaskEnqueueResponse
@@ -9,14 +9,32 @@ from app.auth.models import User
 from typing import Annotated
 from app.sandbox.models import Job, CeleryStatuses
 import uuid
+from app.sandbox.rate_limiter import RateLimiter
 from database import get_db
+import redis
+import redis.asyncio as aioredis
+
+from settings import RATE_LIMIT
+
+r = aioredis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 sandbox_router = APIRouter(prefix='/sandbox')
 
 # @sandbox_router.post('/execute', response_model=TaskEnqueueResponse)
 @sandbox_router.post('/execute')
-async def execute_docker(request: Request, code: Annotated[str, Body(embed=True)], current_user: Annotated[User | None, Depends(get_current_user_optional)] = None, db: AsyncSession = Depends(get_db)):
+async def execute_docker(
+        request: Request,
+        code: Annotated[str, Body(embed=True)],
+        current_user: Annotated[User | None, Depends(get_current_user_optional)] = None,
+        db: AsyncSession = Depends(get_db)
+):
     random_id = uuid.uuid4()
-    return request.client
+    client_id = request.client.host
+    curr = await r.incr(str(client_id))
+
+    if curr == 1:
+        await r.expire(client_id, 60)
+    if curr > int(RATE_LIMIT):
+        raise HTTPException(429)
     user_id = int(current_user.id) if current_user else None
     job = Job(status=CeleryStatuses.PENDING, uuid=str(random_id), user_id=user_id)
     db.add(job)
